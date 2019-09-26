1c3c8860f7820bd505adb2a7cc4cbb2f9b41c258
cougar <upper1@inbox.ru>
2019-09-26 21:18:43 +0300

    Menu
    Добавлено меню для добавления новых записей

--- app/src/main/java/com/sample/cityweather/Activities/MainActivity.kt
+++ app/src/main/java/com/sample/cityweather/Activities/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.sample.cityweather.Activities
 
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.sample.cityweather.Fragments.WeatherListFragment
+import com.sample.cityweather.R
 
 class MainActivity : SingleFragmentActivity(),
     WeatherListFragment.OnListFragmentInteractionListener {
+
+    var mMenu: Menu? = null
 
     override fun onListFragmentInteraction(string: String) {
         //TODO вызвать новое активити
@@ -13,5 +18,21 @@ class MainActivity : SingleFragmentActivity(),
 
     override fun createFragment(): Fragment {
         return WeatherListFragment.newInstance()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        mMenu = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.addItem -> {
+                startActivity(EditActivity.newIntent(this))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
